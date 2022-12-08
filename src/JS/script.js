aba21fff1dedd9134e923a6cb2724ab8497c97d6
Filipe-DLL <filipeferreira.new@gmail.com>
2022-12-07 21:48:22 -0300

    Finalizado

--- src/JS/script.js
+++ src/JS/script.js
@@ -3,7 +3,8 @@ const listaDuvida = document.querySelectorAll(".duvida")
 listaDuvida.forEach(element => {
     let duvidaHeader = element.querySelector(".duvida-header")
 
-    duvidaHeader.addEventListener("click", event => {
+    duvidaHeader.addEventListener("click", event => {                              
+        const ativo = document.querySelector(".exibir")
 
         if (ativo === element) {
             element.classList.remove("exibir")
