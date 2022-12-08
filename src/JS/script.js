ea4de05ed1b4c033069f5d9dcb16053e10c4afa3
Filipe-DLL <filipeferreira.new@gmail.com>
2022-12-07 21:42:14 -0300

    Finalizado

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,13 +1,19 @@
 const listaDuvida = document.querySelectorAll(".duvida")
-console.log(listaDuvida);
 
 listaDuvida.forEach(element => {
     let duvidaHeader = element.querySelector(".duvida-header")
 
     duvidaHeader.addEventListener("click", event => {
-        listaDuvida[0].classList.remove("exibir")
-        listaDuvida[1].classList.remove("exibir")
-        listaDuvida[2].classList.remove("exibir")
+
+        if (ativo === element) {
+            element.classList.remove("exibir")
+            return
+        }
+
+        if (ativo) {
+            ativo.classList.remove("exibir")
+        }
+
         element.classList.add("exibir")
     })
 });
